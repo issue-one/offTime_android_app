125d477894494434b72cbeba60dec2746d60d1ed
BezaT123 <bezat.berhe@gmail.com>
2021-02-03 11:52:54 +0300

    skelton for the project

---
+++ lib/screens/analytics_screen/AnalyticsPage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AnalyticsPage extends StatefulWidget {
+  @override
+  _AnalyticsPageState createState() => _AnalyticsPageState();
+}
+
+class _AnalyticsPageState extends State<AnalyticsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Analytics Page"),
+      ),
+    );
+  }
+}
