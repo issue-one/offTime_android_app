9e0f666a83046b58d4224b3928fd46be5bb42c2b
BezaT123 <bezat.berhe@gmail.com>
2021-02-28 19:04:04 +0300

    Copying from the previous project room files

---
+++ lib/screens/create_room_screen/CreateRoomPage.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:offTime/models/Room.dart';
+
+import '../../models/User.dart';
+import '../screens.dart';
+
+class CreateRoomPage extends StatelessWidget {
+  static String routeName = "/createRoomPage";
+  @override
+  Widget build(BuildContext context) {
+    User user = ModalRoute.of(context).settings.arguments;
+    final _formKey = GlobalKey<FormState>();
+
+    final Map<String, dynamic> _room = {};
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Create Room"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              // Navigator.pop(context, "Successfully created Room");
+            },
+          )
+        ],
+      ),
+      body: Form(
+        key: _formKey,
+        child: Container(
+          color: Theme.of(context).primaryColor,
+          child: Column(
+            children: [
+              TextFormField(
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Enter Room Name';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  labelText: 'Room Name',
+                  icon: Icon(
+                    Icons.person_outline,
+                    size: 30,
+                  ),
+                ),
+                onSaved: (value) {
+                  // setState(
+                  //   () {
+                  //     _room["name"] = value;
+                  //   },
+                  // );
+                },
+              ),
+              // Add password here too?
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 30.0),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.75,
+                  child: MyElevatedButton(
+                      title: "Create Room",
+                      myOnPressed: () {
+                        final form = _formKey.currentState;
+                        if (form.validate()) {
+                          form.save();
+                          // print("herekndks");
+                          Room(
+                              hostUsername: user.username, name: _room["name"]);
+
+                          // final UserAuthenticationEvent event= LoginRequested(
+                          // userInput: UserInput( username: _user["username"], password: _user["password"]));
+                          // print(_user);
+                          // BlocProvider.of<UserAuthenticationBloc>(context).add(event);
+                          // Navigator.of(context).pushNamedAndRemoveUntil(MyStatefulWidget.routeName,(route)=>false );
+                        }
+                      }),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
