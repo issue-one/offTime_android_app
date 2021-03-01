667c367ecef1947d68ac662218bddbfc07628209
Hanna Girma <hannagirma16@gmail.com>
2021-03-01 05:25:27 +0300

    User screens clean up

--- lib/blocs/blocs.dart
+++ lib/blocs/blocs.dart
@@ -1,9 +1,8 @@
 export 'user/user_bloc.dart';
 export 'user/user_event.dart';
 export 'user/user_state.dart';
-export 'authentication/authentication_bloc.dart';
-export 'authentication/authentication_event.dart';
-export 'authentication/authentication_state.dart';
+export 'authentication/authentication.dart';
+
 export 'Analytics/analytics_bloc.dart';
 export 'AnalyticsOnline/analytics_online_bloc.dart';
 export 'room/room_bloc.dart';
